66684e649edd385390691d9b66d3393582dbcf53
sajib689 <jibonraja11@gmail.com>
2025-04-09 16:03:30 +0600

    style: enhance layout and structure of Login component for improved UI

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
+        <div className="register">
+        <div className="card">
+          <div className="card-header">
+            <h3>Login</h3>
+          </div>
+          <div className="card-body">
+            <form action="">
             
+              {/* Email */}
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                  id="email"
+                />
+              </div>
+              {/* Password */}
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                  id="password"
+                />
+              </div>
+             
+              {/* Button */}
+              <div className="form-group">
+                <input type="submit" className="btn" value="Login" />
+              </div>
+              <div className="form-group">
+                <span className="link">
+                  <Link className="text" to='/register'>Have An Account?</Link>
+                </span>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     );
 };
 
