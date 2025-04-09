2c32cb4533d7e02bb512b06479724e918d3ef88c
sajib689 <jibonraja11@gmail.com>
2025-04-09 15:09:00 +0600

    add scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import './main.scss'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/Router';
 
